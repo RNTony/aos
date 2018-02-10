1b2fef1ee12d34d0d7c9b54f0e6453ae33e4578a
RNTony <rajnivxlll@gmail.com>
2018-02-10 13:52:57 +0100

    Ajout de DELETE USERS
    DELETE USERS

--- api/users.php
+++ api/users.php
@@ -36,7 +36,34 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
 else if ($_SERVER['REQUEST_METHOD'] == "PUT") {}
 
+*/
+else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+	 //Tu recuperes l'id du contact
+         $id = $_GET['id'];
+         //Requete SQL pour supprimer le contact dans la base
+        
+        $req = "DELETE FROM users WHERE users_id = '$id'";
+        //execution de la requete et obtention d'un statement
+        $stmt = $pdo->query($req);
 
-else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {}*/
+        //mode de fetch
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+
+        $requete = $pdo->prepare("SELECT * FROM users");
+			$requete->execute();
+			$resultats = $requete->fetchAll();
+			$retour["Utilisateur"]["donnee"] = $resultats; 
+			$retour["Utilisateur"]["nb d'utilisateur"] = count($resultats);
+
+        	retour_json(true,'Données utilisateur',$retour);
+        	http_response_code(200);
+        }
+
+    else {
+    	http_response_code(404);    }
+    
+
+         //Et la tu rediriges vers ta page contacts.php pour rafraichir la liste
+        // header("location:users.php");
 
 ?>
